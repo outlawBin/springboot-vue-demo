b567284739e4e73ee8ca77039d21247671201cc4
outlaw <984483471>
2021-04-30 15:35:44 +0800

    初始化spring boot

---
+++ src/test/java/cn/gues/SpringbootVueDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.gues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootVueDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
